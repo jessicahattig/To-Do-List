1d3df246d268961afc5d9c3de4fc7e3e09e42b27
jessicahattig <jessicahattig@gmail.com>
2023-09-17 10:13:36 -0700

    Add IDs with red and green phase testing.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -80,5 +80,19 @@ namespace ToDoList.Tests
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
+
+  [TestMethod]
+  public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+  {
+    //Arrange
+    string description = "Walk the dog.";
+    Item newItem = new Item(description);
+
+    //Act
+    int result = newItem.Id;
+
+    //Assert
+    Assert.AreEqual(1, result);
+  }
   }
 }

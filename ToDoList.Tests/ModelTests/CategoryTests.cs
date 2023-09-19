b000e90d03542139d588258ba602615b725b97e1
jessicahattig <jessicahattig@gmail.com>
2023-09-18 19:33:26 -0700

    Category.cs, third test, fail. See commented out notes about needing to clear previous tests.

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -28,5 +28,20 @@ namespace ToDoList.Tests
       //Assert
       Assert.AreEqual(name, result);
     }
+
+    [TestMethod]
+    public void GetId_ReturnsCategoryId_Int()
+    {
+      //Arrange
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+
+      //Act
+      int result = newCategory.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+    //Test failed because It says we expected to receive 1 but got an ID of 3 instead. This is because we assign each Category Id by running Id = _instances.Count; in the constructor. The third test is receiving a Category with an Id of 3 because sample Categorys created in previous tests remain in the static _instances list.
   }
 }
